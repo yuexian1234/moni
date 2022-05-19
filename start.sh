08cb62853fee6447b00c7c653963ae71fb163757
yuexian <yuexian>
2022-05-19 12:30:14 +0800

    add moni2

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 kubectl create ns monitoring
  kubectl -n monitoring create configmap prometheus-core --from-file=prometheus.yaml=./prometheus-config.yaml
+ kubectl -n monitoring create configmap prometheus-rules  --from-file=cadvisor-rule.yml=./prometheus-rule.yaml
  ## 创建rule config
   kubectl port-forward -n monitoring prometheus-core-0  --address 0.0.0.0 --address :: 9090:9090
